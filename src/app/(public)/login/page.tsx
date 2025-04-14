6013307ebe938f04190eb03b2c6f6029ae534254
SEBASTIAO RODRIGO <rodrigoexer1@gmail.com>
2025-04-14 10:54:25 -0300

    updated

--- src/app/(public)/login/page.tsx
+++ src/app/(public)/login/page.tsx
@@ -9,7 +9,7 @@ export default function Login(){
     alert('Servidor indisponível')
   }
   return(
-    <div className="bg-green-600 flex flex-col p-10 h-screen">
+    <div className="bg-green-600 flex flex-col p-10 h-screen max-w-[600px]">
 
       <div className="h-96 flex justify-center items-center" data-aos="fade-down" data-aos-delay="500" >
         <Image width={300} height={100} src="/logoPay.png" alt="" />
@@ -17,17 +17,19 @@ export default function Login(){
       
       <div className="flex-1 flex lg:flex-col flex-col mb-20 items-center justify-center gap-3" data-aos="fade-up" data-aos-delay="900" >
         
-        <div className="row flex lg:flex-row flex-col w-full gap-3 mb-3 lg:w-[800px]">
-            <input type="text" placeholder="email@gmail.com" className="p-3 rounded-md border w-full" />
-            <input type="password" placeholder="####################" className="p-3 rounded-md border w-full" />
+        <div className="row flex lg:flex-row flex-col w-full gap-3 mb-3 lg:max-w[600px]">
+            <input type="text" placeholder="email@gmail.com" className="p-3 rounded-md border w-full border-none ouline-none" />
+            <input type="password" placeholder="####################" className="p-3 rounded-md border w-full lg:max-w[400px] border-none ouline-none" />
         </div>
         
+        <div className="row flex lg:flex-row flex-col w-full gap-3 mb-3 lg:max-w[600px]">
         <button onClick={ handleLogin }
-          className="bg-white text-green-800 p-2 w-full lg:w-96 rounded-md font-bold text-2xl">Acessar</button>
+          className="bg-white text-green-800 p-2 w-full md:flex-1 rounded-md font-bold text-2xl">Acessar</button>
         
         <Link href='/'>
-          <button className="border p-2 text-white w-full lg:w-96 rounded-md font-bold text-2xl">Sair da conta</button>
+          <button className="border p-2 text-white w-full md:flex-1 rounded-md font-bold text-2xl">Sair da conta</button>
         </Link>
+        </div>
       </div>
     </div>
   )
--- src/app/(public)/page.tsx
+++ src/app/(public)/page.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 
 export default function Home(){
   return(
-    <div className="bg-green-600 flex flex-col p-10 h-screen">
+    <div className="bg-green-600 flex flex-col p-10 h-screen max-w-[400px]">
       <div className="flex-1 flex justify-center items-center" data-aos="fade-down" data-aos-delay="500" >
         <Image width={300} height={100} src="/logoPay.png" alt="" />
       </div>
@@ -13,7 +13,7 @@ export default function Home(){
       <Link href='/login' className="bg-white text-green-800 p-2 w-full flex items-center justify-center rounded-md font-bold text-2xl"> 
         <button>Acessar</button>
       </Link>
-        <button className="border p-2 text-white w-full rounded-md font-bold text-2xl">Sair da conta</button>
+        <button className="border p-2 text-white w-full rounded-md font-bold text-2xl hover:bg-white hover:text-green-600 duration-300">Sair</button>
       </div>
     </div>
   )
